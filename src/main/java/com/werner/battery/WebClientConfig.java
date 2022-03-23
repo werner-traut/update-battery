f83ee36bdbb98027a7b0a218ccdb282b7e648cc5
Werner Traut <werner.traut@availcarsharing.com>
2022-03-22 18:35:28 -0700

    Update logs

--- src/main/java/com/werner/battery/WebClientConfig.java
+++ src/main/java/com/werner/battery/WebClientConfig.java
@@ -1,10 +1,12 @@
 package com.werner.battery;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
+@Slf4j
 public class WebClientConfig {
 
     private final BatteryProperties batteryProperties;
@@ -15,6 +17,7 @@ public class WebClientConfig {
 
     @Bean
     public WebClient webClient() {
+        log.info("Creating web client with {}", batteryProperties.eAuthToken());
         return WebClient.builder()
                 .baseUrl("https://enlighten.enphaseenergy.com")
                 .defaultHeader("e-auth-token", batteryProperties.eAuthToken())
