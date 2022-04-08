cab7b9e2cba2893a14c27a1992ca746f66212dad
Werner Traut <wtraut@collineargroup.com>
2022-04-08 07:47:38 -0700

    add retry logic for pinging app

--- src/main/java/com/werner/battery/BatteryApplication.java
+++ src/main/java/com/werner/battery/BatteryApplication.java
@@ -1,6 +1,8 @@
 package com.werner.battery;
 
 import lombok.extern.slf4j.Slf4j;
+import reactor.util.retry.Retry;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.DayOfWeek;
-import java.time.LocalDateTime;
+import java.time.Duration;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Objects;
@@ -76,14 +78,20 @@ public class BatteryApplication {
             keepAppUp = false;
         }
         if (keepAppUp) {
-            HttpStatus statusCode = Objects.requireNonNull(webClient.get()
+            try {
+                HttpStatus statusCode = Objects.requireNonNull(webClient.get()
                             .uri("https://update-battery.herokuapp.com/actuator/health")
                             //                .uri("http://localhost:8080/actuator/health")
                             .retrieve()
                             .toEntity(Void.class)
+                            .retryWhen(Retry.fixedDelay(3, Duration.ofSeconds(3)))
                             .block())
                     .getStatusCode();
             log.info("Pinged app and got {} status code", statusCode);
+            } catch (Exception e) {
+                log.error("Error pinging the app", e);
+            }
+            
         }
     }
 
